d350574422dd6e5cc49fd5f5c5c944027f1570fd
ShuLiang <853768742@qq.com>
2021-10-23 17:10:12 +0800

    1提交

---
+++ src/com/alibaba/amoxiang/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.alibaba.amoxiang;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("1 ");
+        System.out.println("xintianja ");
+    }
+}
