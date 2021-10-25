1377cbba8ce07334cec24fca4129772a7c709d04
ShuLiang <853768742@qq.com>
2021-10-25 18:09:30 +0800

    33提交

--- src/com/alibaba/amoxiang/HelloWorld.java
+++ src/com/alibaba/amoxiang/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("2");
-        System.out.println(3);
+        System.out.println(33);
+        System.out.println(33);
     }
 }
